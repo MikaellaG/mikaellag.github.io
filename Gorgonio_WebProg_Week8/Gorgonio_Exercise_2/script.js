4cdc5bcd33e1cd329f23c35995129d937d3774b4
MikaellaG <msGorgonio@mcm.edu.ph>
2025-02-06 17:19:18 +0800

    Add files via upload

---
+++ Gorgonio_WebProg_Week8/Gorgonio_Exercise_2/script.js
@@ -0,0 +1,10 @@
+const person = {
+    name: "Alice",
+    age: 25,
+    hobbies: ["reading", "hiking", "painting"]
+  };
+  
+  const jsonString = JSON.stringify(person);
+  
+  console.log(jsonString);
+  
